0fc0792c3668f18c73234422c12ff2c1c86e2e76
Fabio Vendramin <fabio@techside.it>
2020-04-16 17:35:13 +0200

    Adapt all integration tests to a FeedStoreSpecs protocol

--- Tests/RealmFeedStoreIntegrationTests.swift
+++ Tests/RealmFeedStoreIntegrationTests.swift
@@ -11,7 +11,7 @@ import RealmSwift
 
 extension Realm: RealmAdapter {}
 
-class RealmFeedStoreIntegrationTests: XCTestCase {
+class RealmFeedStoreIntegrationTests: XCTestCase, FeedStoreSpecs {
     
     override func setUp() {
         super.setUp()
@@ -21,62 +21,74 @@ class RealmFeedStoreIntegrationTests: XCTestCase {
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
-        
-        expect(sut, toRetrieve: .empty)
+
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
-    
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
+
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+    }
+
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
         let sut = makeSUT()
-        let feed = uniqueImageFeed()
-        let timestamp = Date()
-        
-        sut.insert(feed, timestamp: timestamp) { _ in }
 
-        expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
+        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
-    
+
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
+
+        assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
+    }
+
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
+
+        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+    }
+
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
+
+        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
+
     func test_insert_overridesPreviouslyInsertedCacheValues() {
         let sut = makeSUT()
-        let firstFeed = uniqueImageFeed()
-        let firstTimestamp = Date()
-        let latestFeed = uniqueImageFeed()
-        let latestTimestamp = Date()
 
-        sut.insert(firstFeed, timestamp: firstTimestamp) { _ in }
-        sut.insert(latestFeed, timestamp: latestTimestamp) { _ in }
-
-        expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+        assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
     }
-    
+
     func test_delete_deliversNoErrorOnEmptyCache() {
         let sut = makeSUT()
-        
-        let deletionError = deleteCache(sut)
-        
-        XCTAssertNil(deletionError)
+
+        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
     }
-    
+
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
+
+        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+    }
+
     func test_delete_deliversNoErrorOnNonEmptyCache() {
         let sut = makeSUT()
-        let feed = uniqueImageFeed()
-        let timestamp = Date()
-        
-        sut.insert(feed, timestamp: timestamp) { _ in }
 
-        let deletionError = deleteCache(sut)
-        
-        XCTAssertNil(deletionError)
+        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
     }
-    
+
     func test_delete_emptiesPreviouslyInsertedCache() {
         let sut = makeSUT()
-        let feed = uniqueImageFeed()
-        let timestamp = Date()
-        
-        sut.insert(feed, timestamp: timestamp) { _ in }
-        deleteCache(sut)
-        
-        expect(sut, toRetrieve: .empty)
+
+        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+    }
+
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+
+        assertThatSideEffectsRunSerially(on: sut)
     }
     
     // MARK: - Helpers
@@ -89,54 +101,6 @@ class RealmFeedStoreIntegrationTests: XCTestCase {
         return sut
     }
     
-    private func expect(_ sut: FeedStore, toRetrieve expectedResult: RetrieveCachedFeedResult, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait to retrieve from realm db")
-        
-        sut.retrieve { retrievedResult in
-            switch (expectedResult, retrievedResult) {
-                case (.empty, .empty),
-                     (.failure, .failure):
-                    break
-                    
-                case let (.found(expectedFeed, expectedTimestamp), .found(retrievedFeed, retrievedTimestamp)):
-                    XCTAssertEqual(retrievedFeed, expectedFeed, file: file, line: line)
-                    XCTAssertEqual(retrievedTimestamp, expectedTimestamp, file: file, line: line)
-                    
-                default:
-                    XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    @discardableResult
-    private func deleteCache(_ sut: FeedStore) -> Error? {
-        let exp = expectation(description: "Waiting to delete cache from realm db")
-        var receivedError: Error?
-        sut.deleteCachedFeed { deletionError in
-            receivedError = deletionError
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-        return receivedError
-    }
-    
-    private func uniqueImageFeed() -> [LocalFeedImage] {
-        return [uniqueImage(), uniqueImage()]
-    }
-    
-    private func uniqueImage() -> LocalFeedImage {
-        return LocalFeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
-    }
-    
-    private func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
     }
